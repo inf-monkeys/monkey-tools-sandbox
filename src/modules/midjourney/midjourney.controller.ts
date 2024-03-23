5d33a78f07ecdffa99459f666e2b60c9a448bda8
codevoyager1984 <codevoyager.jay@gmail.com>
2024-03-23 17:54:04 +0700

    feat: Add goapi midjourney blend api

--- src/modules/midjourney/midjourney.controller.ts
+++ src/modules/midjourney/midjourney.controller.ts
@@ -9,7 +9,11 @@ import {
 import { AuthGuard } from '@/common/guards/auth.guard';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { GoApiMidjourneyInput, MidjourneyService } from './midjourney.service';
+import {
+  GoApiMidjourneyBlendInput,
+  GoApiMidjourneyInput,
+  MidjourneyService,
+} from './midjourney.service';
 
 @Controller('')
 @UseGuards(new AuthGuard())
@@ -81,10 +85,100 @@ export class MidjourneyController {
     },
   ])
   @MonkeyToolExtra({
-    estimateTime: 10,
+    estimateTime: 180,
   })
   public async generateImageByGoApi(@Body() body: GoApiMidjourneyInput) {
     const urls = await this.service.generateImageByGoApi(body);
+    return {
+      result: urls,
+    };
+  }
+
+  @Post('/goapi-midjourney-blend')
+  @ApiOperation({
+    summary: '融图（MJ）',
+    description: '使用 MJ 将多张图片合并为一张图片。',
+  })
+  @MonkeyToolName('goapi_midjourney_blend')
+  @MonkeyToolCategories(['gen-image'])
+  @MonkeyToolIcon('emoji:📷:#98ae36')
+  @MonkeyToolInput([
+    {
+      name: 'images',
+      type: 'file',
+      displayName: '图片列表',
+      typeOptions: {
+        multipleValues: true,
+        accept: '.jpg,.jpeg,.png',
+        // 文件数量限制
+        // multipleValues 为 false 时，下面两个的值不需要填，因为只能为 1
+        minValue: 2,
+        maxValue: 5,
+        maxSize: 1024 * 1024 * 10,
+      },
+    },
+    {
+      name: 'process_mode',
+      type: 'options',
+      displayName: '处理模式',
+      default: 'relax',
+      options: [
+        {
+          name: 'relax',
+          value: 'relax',
+        },
+        {
+          name: 'mixed',
+          value: 'mixed',
+        },
+        {
+          name: 'fast',
+          value: 'fast',
+        },
+        {
+          name: 'turbo',
+          value: 'turbo',
+        },
+      ],
+    },
+    {
+      name: 'dimension',
+      type: 'options',
+      displayName: 'dimension',
+      required: false,
+      options: [
+        {
+          name: 'square',
+          value: 'square',
+        },
+        {
+          name: 'portrait',
+          value: 'portrait',
+        },
+        {
+          name: 'landscape',
+          value: 'landscape',
+        },
+      ],
+    },
+  ])
+  @MonkeyToolOutput([
+    {
+      name: 'result',
+      displayName: '图像 URL 列表',
+      type: 'file',
+      typeOptions: {
+        multipleValues: true,
+        accept: '.jpg,.jpeg,.png',
+        maxSize: 1024 * 1024 * 10,
+      },
+    },
+  ])
+  @MonkeyToolExtra({
+    estimateTime: 180,
+  })
+  public async imageBlendByGoApi(@Body() body: GoApiMidjourneyBlendInput) {
+    const urls = await this.service.imageBlendByGoApi(body);
     return {
       result: urls,
     };
--- src/modules/midjourney/midjourney.service.ts
+++ src/modules/midjourney/midjourney.service.ts
@@ -16,8 +16,57 @@ export interface GoApiMidjourneyInput {
   skip_prompt_check?: boolean;
 }
 
+export interface GoApiMidjourneyBlendInput {
+  images: string[];
+  process_mode: string;
+  dimension?: string;
+}
+
 @Injectable()
 export class MidjourneyService {
+  private async pollResult(task_id: string) {
+    let finished = false;
+    // 四张图汇总在一起的
+    let imageUrl = '';
+    while (!finished) {
+      const { data: fetchData } = await axios.post(
+        'https://api.midjourneyapi.xyz/mj/v2/fetch',
+        {
+          task_id: task_id,
+        },
+      );
+      const { status, task_result } = fetchData;
+      logger.info('GOAPI midjourney 查询状态结果：', task_id, status);
+      finished = status === 'finished';
+      if (finished) {
+        imageUrl = task_result.image_url;
+      } else {
+        await sleep(500);
+      }
+    }
+
+    // 把图切开
+    const tmpFolder = getAndEnsureTempDataFolder();
+    const composedImageFile = path.join(tmpFolder, `${task_id}.png`);
+    await downloadFileTo(imageUrl, composedImageFile);
+    const splitedFiles = await splitImage(
+      composedImageFile,
+      path.join(tmpFolder, task_id),
+    );
+    const result = await Promise.all(
+      splitedFiles.map(async (file, index) => {
+        logger.info('开始上传文件：', file);
+        const s3Helper = new S3Helpers();
+        const url = await s3Helper.uploadFile(
+          fs.readFileSync(file),
+          `workflow/artifact/mj/${task_id}/${index}.jpg`,
+        );
+        return url;
+      }),
+    );
+    return result;
+  }
+
   public async generateImageByGoApi(
     inputData: GoApiMidjourneyInput,
   ): Promise<string[]> {
@@ -48,47 +97,37 @@ export class MidjourneyService {
         },
       );
       const { task_id } = imagineData;
+      return await this.pollResult(task_id);
+    } catch (error: any) {
+      logger.error(error);
+      const errMsg = error?.response?.data?.message || error?.message;
+      throw new Error('生成图片失败：' + errMsg);
+    }
+  }
 
-      let finished = false;
-      // 四张图汇总在一起的
-      let imageUrl = '';
-      while (!finished) {
-        const { data: fetchData } = await axios.post(
-          'https://api.midjourneyapi.xyz/mj/v2/fetch',
-          {
-            task_id: task_id,
+  public async imageBlendByGoApi(
+    inputData: GoApiMidjourneyBlendInput,
+  ): Promise<string[]> {
+    const { images, process_mode = 'relax', dimension } = inputData;
+    if (!config.goapi.apikey) {
+      throw new Error('没有配置 GOAPI_KEY，请联系管理员。');
+    }
+    try {
+      const { data: blendData } = await axios.post(
+        `https://api.midjourneyapi.xyz/mj/v2/blend`,
+        {
+          image_urls: images,
+          process_mode,
+          dimension,
+        },
+        {
+          headers: {
+            'X-API-KEY': config.goapi.apikey,
           },
-        );
-        const { status, task_result } = fetchData;
-        logger.info('GOAPI midjourney 查询状态结果：', task_id, status);
-        finished = status === 'finished';
-        if (finished) {
-          imageUrl = task_result.image_url;
-        } else {
-          await sleep(500);
-        }
-      }
-
-      // 把图切开
-      const tmpFolder = getAndEnsureTempDataFolder();
-      const composedImageFile = path.join(tmpFolder, `${task_id}.png`);
-      await downloadFileTo(imageUrl, composedImageFile);
-      const splitedFiles = await splitImage(
-        composedImageFile,
-        path.join(tmpFolder, task_id),
+        },
       );
-      const result = await Promise.all(
-        splitedFiles.map(async (file, index) => {
-          logger.info('开始上传文件：', file);
-          const s3Helper = new S3Helpers();
-          const url = await s3Helper.uploadFile(
-            fs.readFileSync(file),
-            `workflow/artifact/mj/${task_id}/${index}.jpg`,
-          );
-          return url;
-        }),
-      );
-      return result;
+      const { task_id } = blendData;
+      return await this.pollResult(task_id);
     } catch (error: any) {
       logger.error(error);
       const errMsg = error?.response?.data?.message || error?.message;
